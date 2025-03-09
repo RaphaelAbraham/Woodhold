19f6170478b5deb2d6c3fde24c33b66da266d751
RaphaelAbraham <raphi3a@gmail.com>
2025-03-09 12:03:20 +0100

    working on responsive design

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-scrolldown = document.querySelector(".scrolldown");
-
-window.addEventListener("scroll", (e) => {
-  // console.log(window.scrollY);
-  if (window.scrollY > 777) {
-    scrolldown.style.display = "none";
-  } else {
-    scrolldown.style.display = "block";
-  }
-});
+console.log(window.size)
